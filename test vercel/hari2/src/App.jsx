d72cf3983d81ecebe628a008a77f26e5d5d5b5fb
Ogribas <154720567+Ogribas@users.noreply.github.com>
2024-08-02 01:24:51 +0700

    Add files via upload

---
+++ test vercel/hari2/src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import axios, { Axios } from 'axios'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import InsertData from './pages/insertdata.jsx'
+
+
+
+
+
+function App() {
+  const [dummy,setDummy] = useState([])
+
+  useEffect(()=>{
+    axios.get("http://localhost:3000/insertdata")
+    .then((response)=>{
+      setDummy(response.data)
+    })
+    .catch((e)=>{
+      console.log(`error`)
+    })
+  },[])
+
+  function addDataSewa(){
+    setDummy((prevData)=>{return [...prevData,{
+      tanggalSewa:"31/07/2024",
+      pelanggan:"PT Arkaya Sutera ",
+      noHp:"6281363312349",
+      alatSewa:"KESBOR EXC PT Arkaya Sutera",
+      qty:1,
+      status:"Sedang Disewa",
+      durasi: 3
+    }]})
+  }
+
+  const router = createBrowserRouter([
+    {
+      path:"/insertData",
+      element: <InsertData dummy={dummy} setDummy={setDummy} addDataSewa={addDataSewa}></InsertData>
+    }
+  ]);
+  
+
+  return (
+    <>
+     <RouterProvider router={router}></RouterProvider>
+    </>
+  )
+}
+
+export default App
